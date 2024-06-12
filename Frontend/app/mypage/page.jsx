7d2c4fffbe33456657315b041493d0f2363fa0f3
yeejini <117893976+yeejini@users.noreply.github.com>
2024-06-12 14:09:07 +0900

    add frontend files

---
+++ Frontend/app/mypage/page.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Mypage = () => {
+  return (
+    <h1>Contact page</h1>
+  );
+};
+export default Mypage
---
+++ Frontend/components/Hero.jsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import { Button } from './ui/button'
+import { Download, Send } from 'lucide-react';
+
+
+import {
+  RiBriefcase4Fill,
+  RiTeamFill,
+  RiTodoFill,
+  RiArrowDownSLine,
+} from 'react-icons/ri';
+
+import DevImg from './Devlmg';
+import Badge from './Badge';
+import Socials from './Socials';
+
+const Hero = () => {
+  return (
+    <section className='py-12 xl:py-24 h-[84vh] xl:pt-28 bg-blue-100'>
+      <dic className='container mx-auto'>
+        <div className='flex justify-between gap-x-8'>
+          <div>test</div>
+          <div>image</div>
+        </div>
+      </dic>
+      <div className='hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce'>
+        <RiArrowDownSLine className='text-3xl text-primary' />
+      </div>
+    </section>
+  );
+};
+export default Hero
