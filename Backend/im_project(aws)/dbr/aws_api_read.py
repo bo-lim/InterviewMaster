c73d980cf7f31769f680bc640e2dbf2ac9197bc0
mnmnking <159870773+mnmnking@users.noreply.github.com>
2024-07-09 11:13:28 +0900

    Update aws_api_read.py

--- Backend/im_project(aws)/dbr/aws_api_read.py
+++ Backend/im_project(aws)/dbr/aws_api_read.py
@@ -41,7 +41,7 @@ tb_itm = dynamodb.Table("ITM-PRD-DYN-TBL")
 ########## test ##########
 ##########################
 # 전체 데이터 조회(테스트용)
-@app.get("/get_data")
+@app.get("/dbr/get_data")
 async def get_data():
     data = tb_itm.scan().get('Items', [])
     return {"data": data}
@@ -49,7 +49,7 @@ async def get_data():
 
 
 # UUID값 조회(테스트용)
-@app.get("/get_uuid")
+@app.get("/dbr/get_uuid")
 async def get_uuid():
     unique_id = uuid.uuid4()
     print("UUID 기본값 : ", unique_id, "\nUUID hex값 : ", unique_id.hex)
@@ -65,7 +65,7 @@ async def get_uuid():
 ###########################
 # kakao Login
 # 호출시 auth로 redirect해서 인증 진행
-@app.get("/act/kakao")
+@app.get("/dbr/act/kakao")
 def kakao():
     kakao_client_key = os.getenv("KAKAO_CLIENT_KEY")
     if os.getenv("env") == "eks":
@@ -95,7 +95,7 @@ def kakao():
 #     "refresh_token_expires_in": 5183999
 #   }
 # }
-@app.get("/act/kakao/auth")
+@app.get("/dbr/act/kakao/auth")
 async def kakaoAuth(response: Response, code: Optional[str]="NONE"):
     kakao_client_key = os.getenv("KAKAO_CLIENT_KEY")
     kakao_secret_key = os.getenv("KAKAO_SECRET_KEY")
@@ -183,7 +183,7 @@ async def kakaoAuth(response: Response, code: Optional[str]="NONE"):
 class ItemToken(BaseModel):
     access_token: str
 
-@app.post("/act/kakao/logout")
+@app.post("/dbr/act/kakao/logout")
 def kakaoLogout(item: ItemToken, response: Response):
     try:
         access_token = item.access_token
@@ -209,7 +209,7 @@ def kakaoLogout(item: ItemToken, response: Response):
 # kakao 로그아웃(강제)
 # http://192.168.0.66:8002/act/kakao/kill
 # access_token받아야 로그아웃 처리 가능
-@app.get("/act/kakao/kill/{token}")
+@app.get("/dbr/act/kakao/kill/{token}")
 def kakaokill(token: str, response: Response):
     try:
         # 액세스 토큰(강제 kill)
@@ -240,7 +240,7 @@ def kakaokill(token: str, response: Response):
 # get
 # 입력값 user_id
 # 출력값 user_id, user_inf, user_history
-@app.get("/get_user/{user_id}")
+@app.get("/dbr/get_user/{user_id}")
 async def get_user(user_id: str):
     # info 조회
     itm_user_info = tb_itm.get_item(
@@ -280,7 +280,7 @@ async def get_user(user_id: str):
 # get
 # 입력값 user_id
 # 출력값 user_itv_cnt
-@app.get("/get_newitvcnt/{user_id}")
+@app.get("/dbr/get_newitvcnt/{user_id}")
 async def get_newitvcnt(user_id: str):
     # history 조회
     itm_user_history = tb_itm.get_item(
@@ -301,7 +301,7 @@ async def get_newitvcnt(user_id: str):
 # get
 # 입력값 user_id
 # 출력값 user_id, user_history, itv_info
-@app.get("/get_itv/{user_id}")
+@app.get("/dbr/get_itv/{user_id}")
 async def get_itv(user_id: str):
     # info 조회
     itm_user_info = tb_itm.get_item(
@@ -371,7 +371,7 @@ async def get_itv(user_id: str):
 # get
 # 입력값 user_id, itv_no
 # 출력값 user_id, user_history, itv_info
-@app.get("/get_itv/{user_id}/{itv_no}")
+@app.get("/dbr/get_itv/{user_id}/{itv_no}")
 async def get_itv_detail(user_id: str, itv_no: str):
     # info 조회
     itm_user_info = tb_itm.get_item(
